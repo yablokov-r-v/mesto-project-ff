2dc02cddc7d345b13aca954582fd2f75991d3c07
Roman Yablokov <yablokov.r.v@yandex.ru>
2024-05-13 02:47:03 +0300

    Сдача проектной работы Mesto на JavaScript от 13.05.2024

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+let elementTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+let elementList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(name, link, callback) {
+    const elementCopy = elementTemplate.querySelector('.places__item').cloneNode(true);
+    const templateTitle  = elementCopy.querySelector('.card__title');
+    const templateImg  = elementCopy.querySelector('.card__image');
+    templateTitle.textContent = name;
+    templateImg.setAttribute('src', link);
+    elementList.append(elementCopy);
+    const deleteButton = elementCopy.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', callback);
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    const card = event.target.closest('.places__item');
+    card.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+    createCard(item.name, item.link, deleteCard); 
+});
+
+
+
+
+ 
+
